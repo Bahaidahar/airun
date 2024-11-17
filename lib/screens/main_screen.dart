501ec369456fc54dc95d9e8bac2b62ca73a48bf3
Bahaidahar <krutoyBaha@bk.ru>
2024-11-17 20:47:53 +0500

    all project

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:http_parser/http_parser.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -22,11 +25,14 @@ class _MainScreenState extends State<MainScreen> {
   File? _photoFile;
   bool _isUploading = false;
   String _activeModal = ''; // 'photo' or 'voice'
+  late FlutterTts _flutterTts;
+
   @override
   void initState() {
     super.initState();
     _initializeCamera();
     _speechToText = stt.SpeechToText();
+    _flutterTts = FlutterTts();
   }
 
   Future<void> _initializeCamera() async {
@@ -42,8 +48,7 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   Future<void> _takePhoto() async {
-    if (!_isCameraInitialized || _activeModal.isNotEmpty)
-      return; // Prevent opening another modal
+    if (!_isCameraInitialized || _activeModal.isNotEmpty) return;
     try {
       final image = await _cameraController.takePicture();
       setState(() {
@@ -51,13 +56,59 @@ class _MainScreenState extends State<MainScreen> {
         _photoTaken = true;
         _activeModal = 'photo';
       });
+      // Call the upload method after taking the photo
+      await _uploadPhotoToServer(_photoFile!);
     } catch (e) {
       print('Error taking photo: $e');
     }
   }
 
+  Future<void> _uploadPhotoToServer(File photoFile) async {
+    if (_isUploading) return;
+
+    setState(() {
+      _isUploading = true;
+    });
+
+    try {
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse('http://172.20.10.4:8000/api/v1/ai/gpt-photo'),
+      );
+
+      request.headers.addAll({
+        'accept': 'application/json',
+      });
+
+      request.files.add(await http.MultipartFile.fromPath(
+        'file',
+        photoFile.path,
+        contentType: MediaType('image', 'png'), // Set the MIME type if needed
+      ));
+
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        var responseBody = await response.stream.bytesToString();
+        var responseData = json.decode(responseBody);
+        String responseText = responseData['response'];
+
+        print('Server response: $responseText');
+        _speakResponse(responseText);
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error uploading photo: $e');
+    } finally {
+      setState(() {
+        _isUploading = false;
+        _activeModal = '';
+      });
+    }
+  }
+
   void _startVoiceRecording() async {
-    if (_activeModal.isNotEmpty) return; // Prevent opening another modal
+    if (_activeModal.isNotEmpty) return;
     bool available = await _speechToText.initialize();
     if (available) {
       setState(() {
@@ -79,8 +130,42 @@ class _MainScreenState extends State<MainScreen> {
     _speechToText.stop();
     setState(() {
       _isRecording = false;
-      _activeModal = ''; // Close the modal
+      _activeModal = '';
     });
+    _sendTextToServer();
+  }
+
+  Future<void> _sendTextToServer() async {
+    if (_recordedText.isEmpty) return;
+    try {
+      final response = await http.post(
+        Uri.parse('http://172.20.10.4:8000/api/v1/ai/gpt-text'),
+        headers: {
+          'accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({'text': _recordedText}),
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = json.decode(
+            utf8.decode(response.bodyBytes)); // Decode the bytes to UTF-8
+        String responseText = responseData['response'];
+
+        print(responseText);
+        _speakResponse(responseText);
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error sending request: $e');
+    }
+  }
+
+  void _speakResponse(String responseText) async {
+    FlutterTts flutterTts = FlutterTts();
+    await flutterTts.setLanguage('ru-RU'); // Set language to Russian
+    await flutterTts.speak(responseText); // Speak the response text
   }
 
   @override
@@ -117,7 +202,7 @@ class _MainScreenState extends State<MainScreen> {
                       onPressed: () {
                         setState(() {
                           _photoTaken = false;
-                          _activeModal = ''; // Close the modal
+                          _activeModal = '';
                         });
                         print('Photo sent to server (simulated)');
                       },
